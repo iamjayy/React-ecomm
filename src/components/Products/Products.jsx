36e97bb0fb9449f98b8f98261a5fb386d5da2ee4
iamjayy <jason_doan94@yahoo.com>
2021-01-07 22:33:40 -0500

    commerce products, still need to fix bug with app.js list

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,26 +5,7 @@ import Product from "./Product/Product";
 
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Nike",
-    description: "Off White Jordan 1s",
-    price: "$5",
-    image:
-      "https://i.pinimg.com/originals/d6/af/85/d6af85071c80a85bda4a05c0f8491aeb.jpg",
-  },
-  {
-    id: 2,
-    name: "Macbook",
-    description: "Apple Macbook",
-    price: "$10",
-    image:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-silver-select-201911?wid=892&hei=820&&qlt=80&.v=1572825196932",
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
 
   return (
